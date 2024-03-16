c29e199855440fdff690239a6da19c0e6f296451
Ali zaib <alizaib12220@gmail.com>
2024-03-16 08:10:11 +0500

    Mystorecode

---
+++ admin/mystore.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Home page</title>
+
+    <!-- Bootstrap Linked -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Font Ausome cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+
+<!-- Navbar Start(Developer  Ali zaib) -->
+
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <div class="container-fluid text-white">
+    <a class="navbar-brand" href="#"><h1  class="text-white">My store</h1></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+   
+    <span>
+
+    <i class="fa-solid fa-user-plus"></i>
+    Hello,Admin|
+    <i class="fa-solid fa-right-from-bracket"></i>
+    <a href="#" class="text-decoration-none text-white">Logout</a> |
+  
+    <a href="#" class=  "text-decoration-none text-white">Userpanel</a>
+    </span>
+    
+  </div>
+</nav>
+
+<!-- Navbar End (Developer Ali zaib) -->
+
+
+<!-- Dashboard Start(Developer Ali zaib) -->
+
+<div>
+<h2 class="text-center">Dashboard</h2>
+</div>
+
+
+
+<div class="bg-danger text-center col-md-6 m-auto">
+
+<a href="#" class="text-white  text-decoration-none fs-4 fw-bold px-5" >Add post</a>
+<a href=""  class="text-white  text-decoration-none fs-4 fw-bold px-5" >Users</a>
+</div>
+    
+</body>
+</html>
